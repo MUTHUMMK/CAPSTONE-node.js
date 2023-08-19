2e2d5da63b8381b37ed88d9b62f199856aba91a4
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-08-19 20:53:25 +0530

    build.sh

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,4 @@
 #first create image using dockerfile
 docker build -t muthu:v1.2 .
 
-# login dockerhub & push the image 
-docker login -u $DOCKERHUBUSER -p $DOCKERHUBPSW
-docker tag muthu:v1.2 muthummkdh/nginx:v1.2
-docker push muthummkdh/nginx:v1.2
+
